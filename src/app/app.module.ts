80f42ae8faf0926c800aa2c0db6447a8526306b4
Marek Beneš <marek.benes@robotron.de>
2019-04-03 20:31:25 +0200

    initial

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,12 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { FlexLayoutModule } from '@angular/flex-layout';
+
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
-import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
-
-import { FlexLayoutModule } from '@angular/flex-layout';
+import { FoodComponent } from './food/food.component';
 
 import {
 	MatAutocompleteModule,
@@ -45,7 +45,6 @@ import {
 	MatTooltipModule,
 	MatTreeModule,
 } from '@angular/material';
-import { FoodComponent } from './food/food.component';
 
 const materialModules = [
 	// CdkTableModule,
