779923aadfaabf22bc1c6d6123e12273d0bc4018
Marek Beneš <marek.benes@robotron.de>
2019-08-06 20:42:28 +0200

    budget component created

--- src/app/money-manager/money-manager.module.ts
+++ src/app/money-manager/money-manager.module.ts
@@ -5,16 +5,18 @@ import { Routes, RouterModule } from '@angular/router';
 import { MoneyItemComponent } from './money-item/money-item.component';
 import { MoneyManagerService } from '../common/services';
 import { CoreModule } from '../common/core.module';
+import { BudgetComponent } from './budget/budget.component';
 
 export const moneyRoutes: Routes = [
 	{ path: '', redirectTo: './', pathMatch: 'full' },
-	{ path: 'moneyItem', component: MoneyItemComponent },
+	{ path: 'moneyItem', component: BudgetComponent },
 	{ path: 'moneyItem/:id', component: MoneyItemComponent },
 	{ path: '**', redirectTo: 'moneyItem' }
 ];
 @NgModule({
 	declarations: [
 		MoneyItemComponent,
+		BudgetComponent,
 	],
 	entryComponents: [
 	],
