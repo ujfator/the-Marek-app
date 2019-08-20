9068fe419f1564974133de75774a259c832cd411
Marek Beneš <marek.benes@robotron.de>
2019-08-20 16:24:26 +0200

    try

--- src/app/common/services/workflow-manager.service.ts
+++ src/app/common/services/workflow-manager.service.ts
@@ -27,7 +27,7 @@ export class WorkflowManagerService extends BaseService {
 
 	public loadItems(): void {
 		console.log(environment);
-		this.http.get<WorkflowItemModel[]>(this.route).subscribe((items) => this.items.next(items));
+		this.http.get<WorkflowItemModel[]>('/workflowManager').subscribe((items) => this.items.next(items));
 	}
 
 	public patchItem(item: WorkflowItemModel): void {
