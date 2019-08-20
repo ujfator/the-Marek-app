022528b89d5946f0b2fe4ec114f94a82a2a327fd
Marek Beneš <marek.benes@robotron.de>
2019-08-20 14:27:25 +0200

    trying if server works at all

--- server/server.ts
+++ server/server.ts
@@ -2,6 +2,8 @@ import * as express from 'express';
 import * as bodyParser from 'body-parser';
 import * as commandLineArgs from 'command-line-args';
 
+import { WorkflowManagerRoutes } from 'api/index';
+
 import { Api } from './api';
 
 const server = express();
@@ -26,6 +28,8 @@ const options = commandLineArgs(optionDefinitions);
 const port = process.env.PORT || options[ 'port' ] || 3000;
 server.set('port', port);
 server.listen(port, () => console.log(`Server started on port ${port} with user ${process.env.USERNAME}`))
+
+server.get('api/workflowManager', (req, res) => res.send('hej'));
 
 const api: Api = new Api({ server });
 api.server.use(express.static(__dirname));
