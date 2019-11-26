e49aa3a9a8b6101499eb161ad8cf2fc362feee6a
Marek Beneš <marek.benes@robotron.de>
2019-11-26 19:13:18 +0100

    edtor config changed, booleans into words

--- src/app/quality/quality.component.ts
+++ src/app/quality/quality.component.ts
@@ -17,61 +17,66 @@ import { AddEditQualityItemComponent } from './add-edit-quality-item/add-edit-it
 
 export class QualityComponent {
 
-  displayedColumns: string[] = ['date', 'dayQuality', 'wakeUp', 'goToBed', 'sleepTime', 'mt', 'excercise', 'deepWorkTime' ,'author', 'edit'];
-  qualities: Quality[] = [];
-  allItems: Quality[] = [];
+	displayedColumns: string[] = ['date', 'dayQuality', 'wakeUp', 'goToBed', 'sleepTime', 'mt', 'excercise', 'deepWorkTime' ,'author', 'edit'];
+	qualities: Quality[] = [];
+	allItems: Quality[] = [];
 
-  constructor(
-    protected dialog: MatDialog,
-    protected qualityService: QualityService,
-    protected dialogService: DialogService,
-    protected authorService: AuthorService,
-    protected difficultyService: DifficultyService,
-  ) {
-    this.qualityService.items.subscribe(async (items) => {
-      if (items) {
-        this.allItems = await [...items];
-        if (localStorage.getItem('author')) {
-          this.createDataSource(localStorage.getItem('author'))
-        } else this.qualities = [...items];
-      }
-    });
+	constructor(
+		protected dialog: MatDialog,
+		protected qualityService: QualityService,
+		protected dialogService: DialogService,
+		protected authorService: AuthorService,
+		protected difficultyService: DifficultyService,
+	) {
+		this.qualityService.items.subscribe(async (items) => {
+		if (items) {
+			this.allItems = await [...items];
+			if (localStorage.getItem('author')) {
+				this.createDataSource(localStorage.getItem('author'))
+			} else this.qualities = [...items];
+		}
+		});
 
-    this.authorService.author.subscribe((author) => {
-      if (author && author !== 'Oba') {
-        this.createDataSource(author);
-      } else if (author === 'Oba') this.qualities = [...this.allItems];
-    });
-  }
+		this.authorService.author.subscribe((author) => {
+			if (author && author !== 'Oba') {
+				this.createDataSource(author);
+			} else if (author === 'Oba') this.qualities = [...this.allItems];
+		});
+	}
 
-  addOrEditEntry(entry?: Quality) {
-    const dialogRef = this.dialog.open(AddEditQualityItemComponent, {
-      width: '500px',
-      data: entry || null,
-    });
+	addOrEditEntry(entry?: Quality) {
+		const dialogRef = this.dialog.open(AddEditQualityItemComponent, {
+			width: '500px',
+			data: entry || null,
+		});
 
-    dialogRef.afterClosed().subscribe(result => {
-      result.id ? this.qualityService.patchItem(result) : this.qualityService.addItem(result);
-    });
-  }
+		dialogRef.afterClosed().subscribe(result => {
+			result.id ? this.qualityService.patchItem(result) : this.qualityService.addItem(result);
+		});
+	}
 
-  createDataSource (author?: string) {
-    this.qualities = [];
-    this.allItems && this.allItems.forEach(element => {
-      if (element.author === author) this.qualities.push(element);
-    });
-  }
+	booleanTranslator(bool: boolean) {
+		if (bool) return 'yes';
+		return 'no';
+	}
 
-  delete(entry: Quality) {
-    this.qualityService.deleteItem(entry.id);
-  }
+	createDataSource (author?: string) {
+		this.qualities = [];
+		this.allItems && this.allItems.forEach(element => {
+			if (element.author === author) this.qualities.push(element);
+		});
+	}
 
-  minutes(element): string {
-    if (element) {
-      if (element.duration > 1) {
-        return 'minutes'
-      } else return 'minute'
-    }
-  }
+	delete(entry: Quality) {
+		this.qualityService.deleteItem(entry.id);
+	}
+
+	minutes(element): string {
+		if (element) {
+			if (element.duration > 1) {
+				return 'minutes'
+			} else return 'minute'
+		}
+	}
 
 }
